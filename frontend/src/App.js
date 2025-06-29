a1fa3dabcb6c9ce473909c766787975654467376
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-06-28 21:21:50 -0300

    Initial React front-end app

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+  return (
+    <div className="App">
+      <h1>Rehearsal Scheduler Web App</h1>
+      <p>Coming soon: Schedule, RSVP, chat, setlists, and more.</p>
+    </div>
+  );
+}
+export default App;
